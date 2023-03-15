f66d6776a62bacec7c948fe148941bc0073ffbe7
anika26452 <ayesha.anika1@gmail.com>
2023-03-16 00:51:00 +0600

    BottomNavigationBar Created

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:ticket_booking_app/screen/bottom_bar.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const BottomBar(),
+    );
+  }
+}
+
